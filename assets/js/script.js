14bdbdc9e34d8743daed2ce0fb874d5ca504d4ce
dshaw1242 <david.shaw1242@gmail.com>
2021-04-01 06:58:07 -0600

    hide button when countdown started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 var startBtn = document.getElementById('start');
 var timerEl = document.getElementById('countdown');
 
+// Countdown Timer function
 function coundown(){
     var timeLeft = 75;
+    // hide the button
+    startBtn.style.display = "none";
 
+    // timer interval to count down each second
     var timeInterval = setInterval(function(){
         if(timeLeft > 0){
             timerEl.textContent = timeLeft;
@@ -17,4 +21,6 @@ function coundown(){
     }, 1000);
 }
 
+
+// start countdown when button is clicked
 startBtn.onclick = coundown;
